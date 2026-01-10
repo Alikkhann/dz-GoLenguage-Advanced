704863ed114025ec6461b490c507b89f3f14352b
Alikhan <alikhanmamilov@yandex.ru>
2026-01-10 21:52:13 +0300

    2.1-push

---
+++ 2-random-api/main.go
@@ -0,0 +1,25 @@
+package randomapi
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func randHeaders(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	res := rand.Intn(7)
+	w.Write([]byte(strconv.Itoa(res)))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/randHeader", randHeaders)
+	server := http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
